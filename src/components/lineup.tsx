06634abfeb40533a83505501859b30ee6c76f57e
Rafael Amado <rjamado25.7.2004@gmail.com>
2026-04-27 11:21:49 +0100

    ui(lineup): substituir imagem do flyer pela versão nova

--- src/components/lineup.tsx
+++ src/components/lineup.tsx
@@ -2,7 +2,7 @@ export default function Lineup() {
   return (
     // eslint-disable-next-line @next/next/no-img-element
     <img
-      src="/A5 flyer final (6).png"
+      src="/Design sem nome.png"
       alt="Lineup QUIC Festival 2026"
       className="lineup-img"
     />
